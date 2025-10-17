f297c4620054b4a99839f572e9981d1b80d55345
Sean Behan <codebam@riseup.net>
2025-10-17 00:23:32 -0400

    cleanup

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,11 +1,31 @@
 <script lang="ts">
-    import { Icon } from 'svelte-awesome-icons';
+    import { onMount } from 'svelte';
+
+    let prevScrollpos = 0;
+    let top = '0px';
+
+    onMount(() => {
+        prevScrollpos = window.pageYOffset;
+        window.onscroll = function() {
+            const currentScrollPos = window.pageYOffset;
+            if (prevScrollpos > currentScrollPos) {
+                top = '0px';
+            } else {
+                top = '-100px';
+            }
+            prevScrollpos = currentScrollPos;
+        }
+
+        return () => {
+            window.onscroll = null;
+        }
+    });
 </script>
 
-<header class="fixed top-0 left-0 right-0 bg-gray-900/80 backdrop-blur-md shadow-lg z-50  p-2">
-    <div class="max-w-screen-3xl mx-auto flex justify-between items-center h-12">
+<header class="fixed left-0 right-0 bg-gray-900/80 backdrop-blur-md shadow-lg z-50 p-2 transition-all duration-300" style="top: {top}">
+    <div class="max-w-screen-3xl mx-auto flex justify-between items-center h-12 px-4 md:px-8">
         <div class="flex items-center space-x-4">
-            <a href="/" class="flex items-center">
+            <a href="/" class="flex items-center" sveltekit-preload-data>
                 <h1 class="text-xl font-bold text-gray-100">
                     Serial Mutation <span class="text-blue-400">Engine</span>
                 </h1>
