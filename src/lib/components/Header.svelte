9003b1825214123101c82e649d5e601e81e951a1
Sean Behan <codebam@riseup.net>
2025-10-17 03:39:13 -0400

    fix a bug

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -26,7 +26,7 @@
 <header class="fixed left-0 right-0 bg-gray-900/80 backdrop-blur-md z-50 p-2 transition-all duration-300" style="top: {top}">
     <div class="max-w-screen-3xl mx-auto flex justify-between items-center h-12 px-4 md:px-8">
         <div class="flex items-center space-x-4">
-            <a href="/" class="flex items-center" sveltekit-preload-data>
+            <a href="/" class="flex items-center">
                 <h1 class="text-xl font-bold text-gray-100">
                     Serial Mutation <span class="text-blue-400">Engine</span>
                 </h1>
