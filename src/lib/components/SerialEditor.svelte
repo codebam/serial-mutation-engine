bce90c969ef61e4ff1233780d389efab015459ba
Sean Behan <codebam@riseup.net>
2025-10-18 19:13:33 -0400

    more assets... colors

--- src/lib/components/SerialEditor.svelte
+++ src/lib/components/SerialEditor.svelte
@@ -15,8 +15,9 @@
     };
 
     const assetColors = [
-        'bg-red-200', 'bg-green-200', 'bg-blue-200', 'bg-yellow-200', 'bg-purple-200', 'bg-pink-200',
-        'bg-red-300', 'bg-green-300', 'bg-blue-300', 'bg-yellow-300', 'bg-purple-300', 'bg-pink-300',
+        'bg-blue-100', 'bg-blue-200', 'bg-blue-300', 'bg-blue-400', 'bg-blue-500',
+        'bg-blue-600', 'bg-sky-200', 'bg-cyan-200', 'bg-teal-200', 'bg-indigo-200',
+        'bg-sky-300', 'bg-cyan-300', 'bg-teal-300', 'bg-indigo-300'
     ];
 
     const colorMap = new Map<number, string>();
