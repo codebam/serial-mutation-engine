9c4e612635ea595f446d4cb387c7dd96b214b265
Sean Behan <codebam@riseup.net>
2025-10-21 17:00:32 -0400

    parse more serials and properly

--- src/lib/parser.spec.ts
+++ src/lib/parser.spec.ts
@@ -2,14 +2,10 @@
 import { describe, it, expect } from 'vitest';
 import { parseSerial } from './parser';
 import { encodeSerial } from './encoder';
+import * as fs from 'fs';
 
 describe('parser and encoder roundtrip', () => {
-    const serials = [
-        '@Ug!pHG2__CA%$*B*hq}}VgZg1mAq^^LDp%^iLG?SR+R>og0R',
-        '@Uga`vnFme!K<Ude5RG}7is6q8Z{X=bP4k{M{',
-        '@Uga`vnFme!KGCxo/RG}7?s6!2EQ*%(c5C',
-        '@Uga`vnFme!KJYP^dRG}6%sD7w_s7=j5<w5`',
-    ];
+    const serials = fs.readFileSync('./serials.txt', 'utf-8').split('\n').filter(s => s.length > 0).slice(0, 200);;
 
     serials.forEach((serial, index) => {
         it(`should correctly parse and encode serial #${index + 1}`, () => {
