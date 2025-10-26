0f85061491c563888eaa11be660dc87313f5a34d
Sean Behan <codebam@riseup.net>
2025-10-26 05:11:21 -0400

    proper docs with SSR

--- src/routes/docs/+page.server.ts
+++ src/routes/docs/+page.server.ts
@@ -1,8 +1,17 @@
-export async function load({ fetch }) {
-	const response = await fetch('/docs');
-	const { comments } = await response.json();
+import { parse } from 'comment-parser';
+
+export async function load() {
+	const modules = import.meta.glob(['../api/+server.ts', '../../lib/parser.ts', '../../lib/encoder.ts', '../../lib/custom_parser.ts', '../../lib/types.ts'], { query: '?raw', import: 'default' });
+
+	const allComments = [];
+
+	for (const modulePath in modules) {
+		const fileContent = (await modules[modulePath]()) as string;
+		const parsedComments = parse(fileContent);
+		allComments.push(...parsedComments);
+	}
 
 	return {
-		comments: comments
+		comments: allComments
 	};
 }
